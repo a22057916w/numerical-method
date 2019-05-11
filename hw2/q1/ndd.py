bc770a6188c984ae5887968ca84bf66010569128
a22057916w <a22057916w@gmail.com>
2019-05-12 00:20:35 +0800

    dev hw2

--- hw2/q1/ndd.py
+++ hw2/q1/ndd.py
@@ -5,8 +5,8 @@ import math
 import os
 
 def dividedDiffTable(x, y, tlen):
-    row = tlen
-    col = tlen + 1
+    row = col = tlen
+
     # initialize the table
     DD = [ [ 0 for i in range(col) ] for j in range(row) ]
     for i in range(row):
@@ -18,8 +18,39 @@ def dividedDiffTable(x, y, tlen):
             DD[j][i] = (DD[j + 1][i - 1] - DD[j][i - 1]) / (x[j + i] - x[j])
     return DD
 
+def printTable(DD):
+    col = len(DD) + 1
+
+    # initialize the name of the columns of DD table
+    col_names = []
+    for i in range(col):
+        if i == 0:
+            continue
+        if i < 5:
+            name = "f["
+            for j in range(i):
+                if j > 0:
+                    name += ", "
+                name += "x" + str(j)
+            name += "]"
+        else:
+            name = "f["
+            for j in range(3):
+                if j > 0:
+                    name += ", "
+                name += "x" + str(j)
+            name += "...x" + str(i) + "]"
+        col_names.append(name)
+
+    df = pd.DataFrame(DD)
+    df.columns = col_names  # assign column names to df
+
+    print(df)
+
 if __name__ == "__main__":
     x = [5, 6, 9, 11]
     y = [12, 13, 14, 16]
+    intervals = [(0.9, 13.3)]
     DD = dividedDiffTable(x, y, len(x))
     print(DD)
+    printTable(DD)
